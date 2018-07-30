3251183a226c03319db2a45e99630d0418bf1f03
nelluri <nskanth987@gmail.com>
2018-07-30 09:17:01 +0530

    java test code

---
+++ src/com/java/practice/PracticeConcepts.java
@@ -0,0 +1,29 @@
+package com.java.practice;
+
+public class PracticeConcepts {
+
+    public static void main(String[] args) {
+
+        byte B = (byte)500;
+
+        System.out.println(B);
+    }
+
+}
+
+interface I{
+
+    class A {
+        int variableA = 10;
+
+        void getSN(){
+            System.out.println(variableA);
+        }
+    }
+}
+
+
+
+/*class B extends A {
+    static int variableA = 20;
+}*/
